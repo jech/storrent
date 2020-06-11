99181f1bf097166719626c12f9f4fe2797dfcf18
Juliusz Chroboczek <jch@irif.fr>
2020-06-11 15:22:15 +0200

    Document package mono.

--- mono/mono.go
+++ mono/mono.go
@@ -1,3 +1,4 @@
+// Package mono implements monotonic time in seconds.
 package mono
 
 import (
@@ -11,6 +12,7 @@ func init() {
 	origin = time.Now().Add(-time.Second)
 }
 
+// Time represtents a monotonic time with second granularity.
 type Time uint32
 
 func new(tm time.Time) Time {
@@ -21,6 +23,7 @@ func new(tm time.Time) Time {
 	return Time(d / time.Second)
 }
 
+// Sub returns t1 - t2.
 func (t1 Time) Sub(t2 Time) uint32 {
 	if t1 < t2 {
 		return 0
@@ -28,22 +31,28 @@ func (t1 Time) Sub(t2 Time) uint32 {
 	return uint32(t1) - uint32(t2)
 }
 
+// Before returns true if t1 < t2.
 func (t1 Time) Before(t2 Time) bool {
 	return t1 < t2
 }
 
+// Now returns the current monotonic time, as a number of seconds since an
+// arbitrary origin.
 func Now() Time {
 	return new(time.Now())
 }
 
+// Since returns the number of seconds elapsed since t.
 func Since(t Time) uint32 {
 	return Now().Sub(t)
 }
 
+// LoadAtomic performs an atomic load of a mono.Time.
 func LoadAtomic(addr *Time) Time {
 	return Time(atomic.LoadUint32((*uint32)(addr)))
 }
 
+// StoreAtomic performs an atomic store of a mono.Time.
 func StoreAtomic(addr *Time, val Time) {
 	atomic.StoreUint32((*uint32)(addr), uint32(val))
 }
